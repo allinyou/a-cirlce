3b386ad2105fcc7a9265e5591ff6426550be637c
Jeany <469420741@qq.com>
2018-09-14 17:40:28 +0800

    vue.config

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
     'linebreak-style': ["off", "windows"],
   },
   parserOptions: {
