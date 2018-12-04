19ff913180a2a9bfc2b9873596f6903ad84f6ae5
Jeany <469420741@qq.com>
2018-12-04 15:44:53 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
     logout() {
       localStorage.clear();
       location.reload();
-    }
+    },
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from '@/App';
 import Toast from 'jeany-vue-toast';
-import 'jeany-vue-toast/toast.css';
 import router from './router';
 import store from './store/index';
 import './registerServiceWorker';
